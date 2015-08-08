71747a3e04db76531d9b9d15bc75c277fb02dfe7
Helge <helge.krueger@gmail.com>
2015-08-08 15:21:19 +0200

    Improved version of the script

--- scripts/OSMOAuth.groovy
+++ scripts/OSMOAuth.groovy
@@ -6,16 +6,38 @@ requestTokenUrl = 'https://www.openstreetmap.org/oauth/request_token'
 accessTokenUrl = 'https://www.openstreetmap.org/oauth/access_token'
 authorizeUrl = 'https://www.openstreetmap.org/oauth/authorize'
 
+propertyFile = new File(System.getProperty('user.home') + '/.config/osmauth.properties')
+
+println (System.getProperty('user.home') + '.config/osmauth.properties')
+
 println """
 Script to retrieve oAuth tokens for OpenStreetMap
 For this you first need to register an application in
 your OpenStreetMap settings.
 """
 
-println "Enter your consumer key"
-consumerKey = System.console().readLine()
-println "Enter your consumer secret"
-consumerSecret = System.console().readLine()
+prop = new Properties()
+
+try{
+    prop.load(new FileInputStream(propertyFile))
+} catch (ex) {
+    propertyFile.createNewFile()
+}
+
+consumerKey = prop.getProperty('consumerKey')
+if (!consumerKey) {
+    println "Enter your consumer key"
+    consumerKey = System.console().readLine()
+    prop.setProperty('consumerKey', consumerKey)
+}
+consumerSecret = prop.getProperty('consumerSecret')
+if (!consumerSecret) {
+    println "Enter your consumer secret"
+    consumerSecret = System.console().readLine()
+    prop.setProperty('consumerSecret', consumerSecret)
+}
+
+prop.store(new FileOutputStream(propertyFile), null)
 
 consumer = new DefaultOAuthConsumer(consumerKey, consumerSecret)
 provider = new DefaultOAuthProvider(requestTokenUrl, accessTokenUrl, authorizeUrl)
@@ -29,3 +51,9 @@ provider.retrieveAccessToken(consumer, token)
 
 println "Your token: ${consumer.token}"
 println "Your token secret: ${consumer.tokenSecret}"
+
+prop.setProperty('consumerToken', consumer.token)
+prop.setProperty('consumerTokenSecret', consumer.tokenSecret)
+
+prop.store(new FileOutputStream(propertyFile), null)
+
